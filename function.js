81e620dc82a57b70220a006faee195203d814e28
mamunmal <mamunmal8666@gmail.com>
2025-09-18 12:22:33 +0600

    function parameter

---
+++ function.js
@@ -0,0 +1,14 @@
+// function declaration
+function startFan(){
+
+    console.log('stand up');
+    console.log('walk towards the switch');
+    console.log('press the switch');
+}
+// call the function
+startFan();
+console.log('walk up in the morning');
+startFan();
+console.log('just time lunch do it');
+startFan();
+console.log('watching js tutorial');
---
+++ parameter.js
@@ -0,0 +1,27 @@
+/* function bringSingara(money){
+
+    console.log('mama singara den');
+    console.log(money);
+}
+bringSingara(300);
+console.log('ai nen singara mama');
+ */
+//add(130,150);
+function add(num1, num2){
+
+    console.log('ai number tuie ta jug kor', num1, num2);
+}
+
+//add(130, 150);
+
+function sum(a, b, c, d, e){
+
+    console.log(a, b, c, d, e);
+    var sum = a + b + c + d + e;
+    console.log(sum);
+}
+sum(12, 30, 14, 15);
+
+
+
+
